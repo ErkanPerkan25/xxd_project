c0bdd96f65bd7ddb9516a80167fb776c077856f3
Eric Hansson <erichansson.united@gmail.com>
2024-06-29 11:43:58 +0000

    Started working on trying to get all the hex from the file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+/**************************************
+* Author: Eric Hansson
+* Date:
+* Purpose:
+**************************************/
+use std::error::Error;
+use std::env;
+use std::fs;
+use std::path::Path;
+
+fn main() -> Result<(), Box<dyn Error>>{
+    let args: Vec<_> = env::args().collect();
+    let path = Path::new(&args[2]);
+
+    let file = fs::read_to_string(path)?;
+
+    println!("{:?}", file.as_bytes());
+
+    Ok(())
 }
